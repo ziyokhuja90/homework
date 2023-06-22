0044aec2802efa06fa29545322ab94932c872a5a
ziyokhuja90 <128157154+ziyokhuja90@users.noreply.github.com>
2023-06-22 09:24:47 +0500

    homework lesson 8

--- main.py
+++ main.py
@@ -1,58 +1,53 @@
-# #
-# def myfunc():
-#     """
-#     bu hisoblab beradi
-    
-#     """
-#     num1 = int(input('son1>>> '))
-#     num2 = int(input('son2>>> '))
-#     amal = input('amal>>> ')
-#     if amal == '+':
-#         result = num1 + num2
-#         return result 
-        
-#     elif amal == '-':
-#         result = num1 - num2
-#         return result 
-#     elif amal == '*':
-#         result = num1 * num2
-#         return result 
-#     elif amal == '/':
-#         result = num1 / num2
 
-        
-#         return result
 
-# books = []
 
-# def addBooks(*args):
-#     for i in args:
-#         books.append(i)
+# sonlar = []
+# for i in range(1,10):
+#     sonlar.append(i)
+#     if i == 4:
+#         sonlar.append('bu 5')
+#         break    
     
-        
-# 91 145 33 15 zikirillo
+    
+# print(sonlar)
 
-# addBooks('salom', 'salom2')
-# print(books)
-import random 
-while True:
-    quiz = ['is 2 + 2 = 4?','is 2 + 2 = 5?']
-    x = random.choice(quiz)
+kitoblar = {}
+ 
+nomi = input('nomi>>> ')
+necta = int(input('nechta qushmoqchisiz>>> '))
+
+
+def nameFunc(name,**kwargs):
+    kitoblar[name] = kwargs
     
-    quizInput = input(f"{x} your answer>>> ").lower()
-    def myfunc():
-        tugri = 'true'
-        notugri = 'false'
-        
-        if x == 'is 2 + 2 = 4?' and quizInput == 'ha' or quizInput == 'true':
-            return tugri
-        elif x == 'is 2 + 2 = 5?' and quizInput == 'yuq' or quizInput == 'false':
-            return tugri
-        else:
-            return notugri
     
-    print(myfunc())
-        
+   
+
+if necta == 1:
+    bir = input('muallifi>>> ')
+    nameFunc(name=nomi,muallifi = bir)
+    
+elif necta == 2:
+    bir = input('muallifi>>> ')
+    ikki = input('janr>>> ')
+    nameFunc(name=nomi,muallifi = bir,janr=ikki)
+    
+elif necta == 3:
+    bir = input('muallifi>>> ')
+    ikki = input('janr>>> ')
+    uch = int(input('yili>>> '))
+    nameFunc(name=nomi,muallifi = bir,janr=ikki,yili = uch)
+    
+elif necta == 4:
+    bir = input('muallifi>>> ')
+    ikki = input('janr>>> ')
+    uch = int(input('yili>>> '))
+    tur = int(input('necta sotilgaligi>>> '))
+    nameFunc(name=nomi,muallifi = bir,janr=ikki,yili = uch , sotilgan=tur)
+    
+else:
+    print('sorry i do not understand you!')
     
 
-    
+print(kitoblar)
+
