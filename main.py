8994fdc6c5f90604e55ccd25550b849d7cb8e019
Ziyoxuja <ziyoxuja90@gmail.com>
2023-07-04 11:42:56 +0500

    homework translater

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from func import check
-while True:
-    k = input('kitob nomini kiriting>>> ')
 
-    print(check(k))
+# from googletrans import Translator
+
+# translator = Translator()
+
+# translator.translate('안녕하세요.')
+# from google_translator_simplified import Translator
+
+# print(Translator.get_translation('pl', 'text for translation', 'en')) #'Tekst do tłumaczenia '
+# Translator.get_translation('de', 'tekst do przetłumaczenia', 'pl') #'Text für die Übersetzung '
+# Translator.get_translation('pl', 'text for translation') #'Tekst do tłumaczenia '
+# Translator.get_translation('de', 'tekst do przetłumaczenia') #'Text für die Übersetzung '
+
+
+
+from translate import Translator
+
+while True:
+    ask = input('qaysi suzni tarjima qilmoqchisiz>>> ')
+    # uz = input('uzbek tiliga tarjima qilmoqchimisiz>>>')
+    
+    translator= Translator(to_lang="uz")
+
+    translation = translator.translate(ask)
+
+    print(translation)
     
